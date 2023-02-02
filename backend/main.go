5ef8da93a00405f0408ade3646394356211ed567
Mai Tran <mai.t.tran1743@gmail.com>
2023-02-02 13:13:16 -0500

    Created account database and operations

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,107 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
-
-	http.Handle("/", r)
-	fmt.Println("Starting on http://localhost:8080/")
-	http.ListenAndServe(":8080", r)
+type User struct {
+	gorm.Model
+	Username string
+	Password string
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var users []User
+	db.Find(&users)
+	fmt.Println("{}", users)
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func newUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("New User Endpoint Hit")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	vars := mux.Vars(r)
+	username := vars["Username"]
+	password := vars["Password"]
+
+	db.Create(&User{Username: username, Password: password})
+	fmt.Fprintf(w, "New User Successfully Created")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	vars := mux.Vars(r)
+	username := vars["Username"]
+
+	var user User
+	db.Where("Username = ?", username).Find(&user)
+	db.Delete(&user)
+
+	fmt.Fprintf(w, "Successfully Deleted User")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	vars := mux.Vars(r)
+	username := vars["Username"]
+	password := vars["Password"]
+
+	var user User
+	db.Where("Username = ?", username).Find(&user)
+
+	user.Password = password
+
+	db.Save(&user)
+	fmt.Fprintf(w, "Successfully Updated User")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/users", allUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{username}", deleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{username}/{password}", updateUser).Methods("PUT")
+	myRouter.HandleFunc("/user/{username}/{password}", newUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func initialMigration() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+}
+
+func main() {
+
+	initialMigration()
+	handleRequests()
 }
