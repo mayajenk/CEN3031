7085ab9350dd9e082ff5e8f49a42a65a0e07b592
Mai Tran <mai.t.tran1743@gmail.com>
2023-02-03 15:30:12 -0500

    Pass data using JSON instead of URL variables

--- backend/main.go
+++ backend/main.go
@@ -15,13 +15,13 @@ type User struct {
 	gorm.Model
 	Username string
 	Password string
-	isTutor  bool
+	IsTutor  bool
 }
 
-func allUsers(w http.ResponseWriter, r *http.Request) {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+func getAllUsers(w http.ResponseWriter, req *http.Request) {
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect database")
 	}
 
 	var users []User
@@ -31,70 +31,106 @@ func allUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-func newUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("New User Endpoint Hit")
+func getUser(w http.ResponseWriter, req *http.Request) {
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	fmt.Println("Get User Endpoint Hit")
+
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect database")
 	}
 
-	vars := mux.Vars(r)
-	username := vars["username"]
-	password := vars["password"]
+	userID := mux.Vars(req)["id"]
 
-	db.Create(&User{Username: username, Password: password})
+	var user User
+
+	db.First(&user, userID)
+
+	fmt.Println(user)
+	json.NewEncoder(w).Encode(user)
+
+}
+
+func newUser(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("New User Endpoint Hit")
+
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	decoder.DisallowUnknownFields()
+	var user User
+	err = decoder.Decode(&user)
+	if err != nil {
+		panic(err)
+	}
+
+	db.Create(&user)
 	fmt.Fprintf(w, "New User Successfully Created")
 }
 
-func deleteUser(w http.ResponseWriter, r *http.Request) {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+func deleteUser(w http.ResponseWriter, req *http.Request) {
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect database")
 	}
 
-	vars := mux.Vars(r)
-	username := vars["username"]
+	userID := mux.Vars(req)["id"]
 
 	var user User
-	db.Where("username = ?", username).Find(&user)
+	db.First(&user, userID)
+
 	db.Delete(&user)
 
 	fmt.Fprintf(w, "Successfully Deleted User")
 }
 
-func updateUser(w http.ResponseWriter, r *http.Request) {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+func updateUser(w http.ResponseWriter, req *http.Request) {
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect database")
 	}
 
-	vars := mux.Vars(r)
-	username := vars["username"]
-	password := vars["password"]
+	userID := mux.Vars(req)["id"]
 
 	var user User
-	db.Where("username = ?", username).Find(&user)
+	db.First(&user, userID)
 
-	user.Password = password
+	var newUser User
+	decoder := json.NewDecoder(req.Body)
+	decoder.DisallowUnknownFields()
+
+	err = decoder.Decode(&newUser)
+	if err != nil {
+		panic(err)
+	}
+
+	user.Username = newUser.Username
+	user.Password = newUser.Password
+	user.IsTutor = newUser.IsTutor
 
 	db.Save(&user)
 	fmt.Fprintf(w, "Successfully Updated User")
 }
 
 func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/users", allUsers).Methods("GET")
-	myRouter.HandleFunc("/user/{username}", deleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/{username}/{password}", updateUser).Methods("PUT")
-	myRouter.HandleFunc("/user/{username}/{password}", newUser).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/users", getAllUsers).Methods("GET")
+	router.HandleFunc("/users", newUser).Methods("POST")
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+
+	fmt.Printf("Starting on http://localhost:8080\n")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func initialMigration() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Failed to connect database")
 	}
 
 	// Migrate the schema
@@ -102,7 +138,6 @@ func initialMigration() {
 }
 
 func main() {
-
 	initialMigration()
 	handleRequests()
 }
