1205de074596c9ad42d83e92068105ef08ae5670
Mai Tran <mai.t.tran1743@gmail.com>
2023-02-18 15:41:26 -0500

    Automigrate database when the backend starts

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
 	host := "127.0.0.1:8080"
 	if err := http.ListenAndServe(host, httpHandler()); err != nil {
 		log.Fatalf("Failed to listen on %s: %v", host, err)
