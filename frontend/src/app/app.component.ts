d5a7ee940f941e2dcdd61a3acbfdc6b5bbc34373
mayajenk <92805893+mayajenk@users.noreply.github.com>
2023-02-24 14:50:14 -0500

    working on button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  swtichIntroScreen() {
+    // if b2 == Create acount
+    document.getElementById("intro").innerHTML = "Welcome";
+    // switch button labels
+    // add teacher or student check
+
+    // else switch to login screen  
+  }
 }
