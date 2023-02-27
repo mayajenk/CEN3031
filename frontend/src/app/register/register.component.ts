0409f0de8b9f140054cbff29cc600151c4db0c40
mayajenk <92805893+mayajenk@users.noreply.github.com>
2023-02-27 14:34:07 -0500

    register connection

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterService } from '../register.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent {
-  Register() {
+  formData: {
+    username: string,
+    password: string,
+    tutor: boolean
+  } = {
+    username: '',
+    password: '',
+    tutor: false
+  };
+  
+  constructor(private registerService : RegisterService) {}
+
+  register(form: NgForm) {
     // check if username and password entered are valid
     // if username or password is invalid -> display message
     // check if username is already taken
     // if user name is already taken -> display message
     // if successful -> display message
+    this.registerService.register(this.formData.username, this.formData.password, this.formData.tutor)
+      .subscribe(response => {
+        console.log(response);
+      }, error => {
+        console.error(error);
+      });
   }
 }
