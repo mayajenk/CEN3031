3364e01c97ec4fbe86c9e5ea14932d1ea7186044
mayajenk <92805893+mayajenk@users.noreply.github.com>
2023-02-27 14:37:08 -0500

    update register.service.ts

--- frontend/src/app/register.service.ts
+++ frontend/src/app/register.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private registerURL = "/api/register";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(username: string, password: string, tutor: boolean) {
+    const body = {username, password, tutor};
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.post(this.registerURL, body, {headers});
+  }
 }
