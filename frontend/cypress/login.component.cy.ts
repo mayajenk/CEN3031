108417ee2fd572120c79839b2fb1eb13b798a0d3
mayajenk <92805893+mayajenk@users.noreply.github.com>
2023-02-28 14:06:03 -0500

    cypress files

---
+++ frontend/cypress/login.component.cy.ts
@@ -0,0 +1,5 @@
+describe('login.component.cy.ts', () => {
+  it('login successful', () => {
+    // is username & password are returned in a cookie
+  })
+})
---
+++ frontend/cypress/register.component.cy.ts
@@ -0,0 +1,5 @@
+describe('frontend.cy.ts', () => {
+  it('register successful', () => {
+    // if user was able to register
+  })
+})
