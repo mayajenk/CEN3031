75d3e675d3a414682c3505a395619db98d84bc98
mayajenk <92805893+mayajenk@users.noreply.github.com>
2023-03-25 10:20:23 -0400

    logout

--- frontend/src/app/logout/logout.component.ts
+++ frontend/src/app/logout/logout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LogoutService } from '../logout.service';
+import { NgForm } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-logout',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./logout.component.sass']
 })
 export class LogoutComponent {
+  @Input() formData: {
+    username: string,
+    password: string
+  } = {
+    username: '',
+    password: ''
+  };
 
+  private cookieValue: string | undefined;
+
+  constructor(private logoutService : LogoutService, private cookieService: CookieService) {}
+
+  logout(form: NgForm) {
+    // if loggedin, logout
+  }
 }
