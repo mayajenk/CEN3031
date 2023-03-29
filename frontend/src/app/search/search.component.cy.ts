ee7509f6b13bbe87f2d1c46877bf4a44b8ff8a90
Lin-Selina <92886791+Lin-Selina@users.noreply.github.com>
2023-03-29 15:23:23 -0400

    Changed cypress test for search

--- frontend/src/app/search/search.component.cy.ts
+++ frontend/src/app/search/search.component.cy.ts
@@ -9,44 +9,14 @@ describe('search.component.cy.ts', () => {
       imports: [MatSelectModule, MatFormFieldModule, BrowserAnimationsModule]
     })
   })
-  it('should display three options: reading, math, and science', () => {
+  it('should display a search bar and a "Search! button', () => {
     
     cy.mount(SearchComponent, {
       imports: [MatSelectModule, MatFormFieldModule, BrowserAnimationsModule]
     })
 
-    // Get the drop-down menu element
-    const select = cy.get('mat-select');
+    cy.get('mat-form-field').should('exist');
+    cy.get('input[name=search]').should('exist');
+    cy.get('button[type=submit]').should('exist').contains('Search!');
 
-    // Click on the drop-down menu to open it
-    select.click();
-
-    // Get the options within the drop-down menu
-    const options = select.get('mat-option');
-
-    // Check that there are three options
-    options.should('have.length', 3);
-
-    // Check that the options have the correct text
-    options.eq(0).should('have.text', 'Reading');
-    options.eq(1).should('have.text', '');
-    options.eq(2).should('have.text', 'Science');
   });
-
-  it('should allow the user to select an option', () => {    
-
-    cy.mount(SearchComponent, {
-      imports: [MatSelectModule, MatFormFieldModule, BrowserAnimationsModule]
-    })
-
-    // Get the drop-down menu element
-    const select = cy.get('mat-select');
-
-    // Select the "Math" option
-    select.click();
-    cy.get('mat-option').contains('Math').click();
-
-    // Check that the selected value is "math"
-    select.should('have.value', 'math');
-  });
-});
