f69ed2498172e7a17a9eaa6d05dcc4ebdd3006fd
Lin-Selina <92886791+Lin-Selina@users.noreply.github.com>
2023-04-16 21:34:52 -0400

    Temporary hard coded data for search

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Tutor {
+  name: string;
+  subject: string;
+}
+
 
 @Component({
   selector: 'app-search',
@@ -8,14 +13,33 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./search.component.sass']
 })
 export class SearchComponent {
-  @Input() formData: {
-    search : string
-    } = {
-    search : ''
-  };
+  // @Input() formData: {
+    // search : string
+    // } = {
+    // search : ''
+  // };
   
-  search(form: NgForm) {
+  // search(form: NgForm) {
     // list available tutors based on search
     // ignore capitilization and spaces
+  // }
+  
+  searchSubject: string = '';
+  tutors: Tutor[] = [
+    { name: 'John Doe', subject: 'Mathematics' },
+    { name: 'Jane Smith', subject: 'English' },
+    { name: 'Bob Johnson', subject: 'History' },
+    { name: 'Sara Lee', subject: 'Chemistry' },
+    { name: 'Mark Davis', subject: 'Physics' },
+    { name: 'Emily Jones', subject: 'Biology' },
+    { name: 'David Lee', subject: 'Mathematics' },
+    { name: 'Lisa Chen', subject: 'Mathematics' },
+    { name: 'Mike Brown', subject: 'Mathematics' },
+  ];
+
+  filteredTutors: Tutor[] = [];
+
+  filterTutors() {
+    this.filteredTutors = this.tutors.filter(tutor => tutor.subject === this.searchSubject);
   }
 }
